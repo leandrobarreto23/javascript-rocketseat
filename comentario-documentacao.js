f6e10894d87d51ccaeb196e5964522610d3c5a4c
Leandro Barreto <166774589+leandrobarreto23@users.noreply.github.com>
2024-12-26 16:16:04 -0300

    add comentario-documentacao.js

---
+++ comentario-documentacao.js
@@ -0,0 +1,15 @@
+// Comentario de documentacao em JS (Sintaxe de JSDoc). O JSDoc é um padrao para incorporar documentacao no código-fonte a partir desses comentários.
+
+/**
+ * Authenticates the user
+ * @param {String} email user email.
+ * @param {String} password more than 6 characters.
+ * @returns {Number} user id.
+ */
+function signIn(email, password) {
+  //fluxo de autenticacao do usuario
+
+  return 7
+}
+
+signIn("rodrigo@email.com", "12345678")
