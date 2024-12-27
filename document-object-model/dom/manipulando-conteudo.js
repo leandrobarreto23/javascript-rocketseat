74970320181d9d068161b002aaf01a6cf5c29227
Leandro Barreto <166774589+leandrobarreto23@users.noreply.github.com>
2024-12-27 14:53:38 -0300

    add manipulando-conteudo.js

---
+++ document-object-model/dom/manipulando-conteudo.js
@@ -0,0 +1,9 @@
+const guest = document.querySelector("#guest-1")
+
+// retorna o conteudo como texto
+// console.log(guest.textContent)
+// guest.textContent = "Maria"
+
+console.log(guest.textContent) // retorna o conteudo visivel e oculto
+console.log(guest.innerText) // retorna somente o conteudo visivel
+console.log(guest.innerHTML) // retorna o html como texto
