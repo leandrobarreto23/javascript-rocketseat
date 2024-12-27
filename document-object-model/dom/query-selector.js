a90b7775d577555968d7cc9928808d55a0bb312c
Leandro Barreto <166774589+leandrobarreto23@users.noreply.github.com>
2024-12-27 11:57:52 -0300

    add query selector

---
+++ document-object-model/dom/query-selector.js
@@ -0,0 +1,7 @@
+// Acessa o elemento pelo selector ID
+const guest = document.querySelector("#guest-1")
+console.log(guest)
+
+// Retorna todos os elementos encontrados pelo query
+const guests = document.querySelectorAll(".guest")
+console.log(guests)
