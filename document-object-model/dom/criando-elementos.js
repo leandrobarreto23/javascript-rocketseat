96ef3779e558de272e839086dc5c114623c8aee4
Leandro Barreto <166774589+leandrobarreto23@users.noreply.github.com>
2024-12-28 08:43:46 -0300

    finished criando-elementos.js

--- document-object-model/dom/criando-elementos.js
+++ document-object-model/dom/criando-elementos.js
@@ -1,6 +1,7 @@
 const guests = document.querySelector("ul")
 
 const newGuest = document.createElement("li")
+newGuest.classList.add("guest")
 
 const guestName = document.createElement("span")
 guestName.textContent = "Diego"
@@ -9,8 +10,12 @@ const guestSurname = document.createElement("span")
 guestSurname.textContent = "Fernandes"
 
 // adiciona após o ultimo filho
-newGuest.append(guestName)
-// adiciona antes do primeiro filho
-newGuest.prepend(guestSurname)
+// newGuest.append(guestName)
 
-console.log(newGuest)
+// adiciona antes do primeiro filho
+// newGuest.prepend(guestSurname)
+
+// é mais simpels que o append e aceita apenas um argumento
+newGuest.appendChild(guestName)
+
+guests.append(newGuest)
